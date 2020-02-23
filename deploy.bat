30947a2e5236dfeaf94cd315e4cf7eda6fe13ec0
Michiel Devriese <michieldevriese@gmail.com>
2020-02-23 13:53:19 +0100

    Tabs vs spaces
    (cherry picked from commit 6a40133bde10e12b6ec0e52982b7f5e17bb68cfb)

--- deploy.bat
+++ deploy.bat
@@ -4,7 +4,7 @@ SET PWD=%~dp0/
 
 docker run -it --init --rm ^
     --volume %PWD%/:/var/build/ ^
-	--volume %USERPROFILE%/.m2/:/$HOME/.m2/ ^
+    --volume %USERPROFILE%/.m2/:/$HOME/.m2/ ^
     --workdir /var/build ^
     maven:3.6.3-jdk-11 ^
     mvn deploy --settings /$HOME/.m2/settings.xml -Dmaven.repo.local=dependencies

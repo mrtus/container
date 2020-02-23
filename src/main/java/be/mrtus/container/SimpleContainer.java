0adc85a1a2c1e657a503b67327c1808ee961820a
Michiel Devriese <michieldevriese@gmail.com>
2020-02-23 14:07:17 +0100

    Add tests & add missing use-case scenarios

--- src/main/java/be/mrtus/container/SimpleContainer.java
+++ src/main/java/be/mrtus/container/SimpleContainer.java
@@ -2,6 +2,7 @@ package be.mrtus.container;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 final public class SimpleContainer implements ContainerRegistry {
 
@@ -9,11 +10,16 @@ final public class SimpleContainer implements ContainerRegistry {
 
 	@Override
 	public <T> T get(Class<T> serviceClass) {
+		Objects.requireNonNull(serviceClass, "Service class cannot be null");
+
 		return (T)this.serviceInstances.get(serviceClass);
 	}
 
 	@Override
 	public <T> void register(Class<T> serviceClass, T serviceIstance) {
+		Objects.requireNonNull(serviceClass, "Service class cannot be null");
+		Objects.requireNonNull(serviceIstance, "Service instance cannot be null");
+
 		this.serviceInstances.put(serviceClass, serviceIstance);
 	}
 }

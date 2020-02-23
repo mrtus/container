e61aafbc401d295d265c53dfad8d8b7490550280
Michiel Devriese <michieldevriese@gmail.com>
2020-02-23 12:11:15 +0100

    Introduce a simple container

---
+++ src/main/java/be/mrtus/container/Simplecontainer.java
@@ -0,0 +1,19 @@
+package be.mrtus.container;
+
+import java.util.HashMap;
+import java.util.Map;
+
+final public class Simplecontainer implements RegisterableServicesContainer {
+
+	private final Map<Class, Object> serviceInstances = new HashMap<>();
+
+	@Override
+	public <T> T get(Class<T> serviceClass) {
+		return (T)this.serviceInstances.get(serviceClass);
+	}
+
+	@Override
+	public <T> void register(Class<T> serviceClass, T serviceIstance) {
+		this.serviceInstances.put(serviceClass, serviceIstance);
+	}
+}
